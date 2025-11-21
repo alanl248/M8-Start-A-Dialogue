1c3303164fae2060046c692cf752526ce5081efc
alanl248 <alanly71103@gmail.com>
2025-11-21 11:56:40 -0800

    M8L5

--- lessons/dialogue.gd
+++ lessons/dialogue.gd
@@ -11,11 +11,7 @@ var bodies := {
 	"pink": preload ("res://assets/pink.png")
 }
 
-## An array of dictionaries. Each dictionary has four properties:
-## - expression: a [code]Texture[/code] containing an expression
-## - text: a [code]String[/code] containing the text the character says
-## - character: a [code]Texture[/code] representing the character, that we extract from the [code]bodies[/code] dictionary
-## - choices: a [code]dictionary[/code] with [code]String[/code] keys and [code]int[/code] values
+
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
@@ -67,43 +63,28 @@ var dialogue_items: Array[Dictionary] = [
 func _ready() -> void:
 	show_text(0)
 
-## Draws the selected text
 ## [param current_item_index] Displays the currently selected index from the dialogue array
 func show_text(current_item_index: int) -> void:
-	# We retrieve the current item from the array
 	var current_item := dialogue_items[current_item_index]
-	# from the item, we extract the properties.
-	# We set the text to the rich text control
-	# And we set the appropriate expression texture
+	
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
 	body.texture = current_item["character"]
 	create_buttons(current_item["choices"])
-	# We set the initial visible ratio to the text to 0, so we can change it in the tween
 	rich_text_label.visible_ratio = 0.0
-	# We create a tween that will draw the text
 	var tween := create_tween()
-	# A variable that holds the amount of time for the text to show, in seconds
-	# We could write this directly in the tween call, but this is clearer.
-	# We will also use this for deciding on the sound length
+	
 	var text_appearing_duration: float = current_item["text"].length() / 30.0
-	# We show the text slowly
+	
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
-	# We randomize the audio playback's start time to make it sound different
-	# every time.
-	# We obtain the last possible offset in the sound that we can start from
+	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
-	# We pick a random position on that length
 	var sound_start_position := randf() * sound_max_offset
-	# We start playing the sound
 	audio_stream_player.play(sound_start_position)
-	# We make sure the sound stops when the text finishes displaying
 	tween.finished.connect(audio_stream_player.stop)
 
-	# We animate the character sliding in.
 	slide_in()
 
-	# We disable the buttons, and re-enable them after the text is shown
 	for button: Button in action_buttons_v_box_container.get_children():
 		button.disabled = true
 	tween.finished.connect(func() -> void:
@@ -111,27 +92,19 @@ func show_text(current_item_index: int) -> void:
 			button.disabled = false
 	)
 
-## Adds buttons to the buttons container
-## [param choices_data] A dictionary of [String] keys where each key represents
-##                      a sentence that the player can select, and each [int] value
-##                      represents a key for the next text item.
+
 func create_buttons(choices_data: Dictionary) -> void:
 	# We remove all previous buttons
 	for button in action_buttons_v_box_container.get_children():
 		button.queue_free()
-	# We loop over all the dictionary keys
 	for choice_text in choices_data:
 		var button := Button.new()
 		action_buttons_v_box_container.add_child(button)
 		button.text = choice_text
-		# We extract the target line index from the dictionary value
 		var target_line_idx: int = choices_data[choice_text]
 		if target_line_idx == - 1:
-			# If the target line index is -1, we want to quit
 			button.pressed.connect(get_tree().quit)
 		else:
-			# Otherwise we bind the target line index to the show_text function
-			# and use that in the pressed signal's connection
 			button.pressed.connect(show_text.bind(target_line_idx))
 
 ## Animates the character when they start talking
